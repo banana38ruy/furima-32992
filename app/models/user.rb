e357036e3cb9428996922daf53e76fac85db78da
banana38ruy <banana38ruy@yahoo.co.jp>
2021-01-04 12:44:45 +0900

    バリデーションとサインアップとサインインのview遷移の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+class User < ApplicationRecord 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+         
+         validates :nick_name, presence: true
+  
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+         end
+
+         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字でカナを使用してください' } do
+          validates :first_name_kana
+          validates :last_name_kana
+         end
+
+         validates :birthday,presence: true
+
+        end
+

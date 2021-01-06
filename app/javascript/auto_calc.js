feb4e2fa32cbef4a33df9a34296df16c075ff823
banana38ruy <banana38ruy@yahoo.co.jp>
2021-01-06 17:47:57 +0900

    商品出品機能を実装する

---
+++ app/javascript/auto_calc.js
@@ -0,0 +1,12 @@
+function auto_calc (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML =  Math.floor(inputValue / 10)
+    const addTotal = document.getElementById("profit")
+    addTotal.innerHTML = (inputValue - addTaxDom.innerHTML)
+  })
+}
+
+window.addEventListener('load', auto_calc);
---
+++ app/models/item.rb
@@ -0,0 +1,32 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :delivery_burden
+  belongs_to :prefecture
+  belongs_to :shipping
+  
+  
+  
+  belongs_to :user
+  has_one_attached :image
+
+ 
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :expranation
+    validates :price
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000, message: '上記の販売価格を半角数字で入力してください' }
+    
+  #  with_options numericality: { other_than: 1 } do
+    validates :category_id, numericality: { other_than: 1 }
+    validates :quality_id, numericality: { other_than: 1 }
+    validates :delivery_burden_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :shipping_id, numericality: { other_than: 1 }
+    # end
+end

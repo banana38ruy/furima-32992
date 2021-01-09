9b714fc68ee84f730c2db538cd018c82435bad01
banana38ruy <banana38ruy@yahoo.co.jp>
2021-01-09 12:17:45 +0900

    - 商品削除機能を実装する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update] 
+  before_action :set_item, only: [:edit, :show, :update, :destroy] 
   before_action :authenticate_user!, except: [:index, :show]
   
   def index
@@ -17,6 +17,11 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def edit

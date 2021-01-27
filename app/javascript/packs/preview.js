925333d0581cadd5d24982753467fb8174742603
banana38ruy <banana38ruy@yahoo.co.jp>
2021-01-27 22:11:04 +0900

    プレビュー機能の実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,10 +1,11 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ ) || document.URL.match( /items/ )){
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
 
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
+      imageElement.id = "currentThumb"
 
       // 表示する画像を生成
       const blobImage = document.createElement('img');
@@ -17,7 +18,7 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 
     document.getElementById('item-image').addEventListener('change', function(e){
       // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('img');
+      const imageContent = document.querySelector('#currentThumb');
       if (imageContent){
         imageContent.remove();
       }
